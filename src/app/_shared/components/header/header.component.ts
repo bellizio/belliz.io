6497156fc2b7932b742aad80bfe43afb64a94237
Devon Bellizio <jdbellizio@gmail.com>
2018-07-06 15:52:24 -0400

    refactor to remove check for intro element

--- src/app/_shared/components/header/header.component.ts
+++ src/app/_shared/components/header/header.component.ts
@@ -12,15 +12,11 @@ export class HeaderComponent {
     const el = document.getElementById(section);
 
     if (el) {
-      if (section === 'intro') {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-      } else {
-        el.scrollIntoView({
-          behavior: 'smooth',
-          block: 'end',
-          inline: 'nearest'
-        });
-      }
+      el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest'
+      });
     }
   }
 }
