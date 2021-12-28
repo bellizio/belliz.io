37330ed93b6ad9c5433ff153701acf97265b0aca
bellizio <jdbellizio@gmail.com>
2021-12-28 11:24:48 -0600

    update about

--- src/components/about.js
+++ src/components/about.js
@@ -5,27 +5,19 @@ const About = () => (
     id="about"
     className="pv4 ph3 flex flex-column items-center justify-center page-section"
   >
-    <h1 className="f2 f1-m f1-l fw5 mt0 mb4 mb5-ns near-black">About Me</h1>
+    <h1 className="f2 f1-m f1-l fw5 mt0 mb4 mb5-ns near-black">About</h1>
     <p className="ma0 f5 f4-ns fw4 fw3-ns near-black lh-copy measure-wide">
-      My name is <span className="b">Devon Bellizio</span> and I am currently a
-      software engineer at{' '}
-      <a
-        className="blue underline-animate-down"
-        href="https://www.lowes.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Lowe's
-      </a>{' '}
-      in Charlotte, NC. As an engineer, I enjoy architecting and building
-      modular, scalable, software systems that are both performant and easy to
-      use.
+      My name is <span className="b">Devon Bellizio</span> and I am a software
+      engineer based in Charlotte, NC. I have 8+ years of professional
+      experience developing solutions for both small and large scale companies.
+      I am proficient in full-stack web development with a focus on architecting
+      and building front-end systems that are scalable, fast, and easy to use.
       <br />
       <br />
       I'm passionate about learning how stuff works inside and out. Since a
       young age, I have been taking things apart and putting them back together.
-      I think it has helped me solve challenging problems of varying scale on
-      both an individual level and in collaborative environments. <br />
+      This experience has helped me solve challenging problems of varying scale
+      on both an individual level and in collaborative environments. <br />
       <br />
       When I am not behind the keyboard, I enjoy spending time restoring,
       repairing, and riding motorcycles on and off road. One day I want to
