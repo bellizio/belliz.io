3c0c9c042ce9846da4982436c524c09cedb4a292
bellizio <jdbellizio@gmail.com>
2023-01-12 10:14:06 -0500

    remove bentley systems from work section; add vimeo to work section

--- src/components/work.js
+++ src/components/work.js
@@ -4,9 +4,9 @@ import jabilLogo from '../images/jabil-logo.png';
 import qsfLogo from '../images/qsf-logo.png';
 import boaLogo from '../images/boa-logo.png';
 import chsLogo from '../images/chs-logo.png';
-import bentleyLogo from '../images/bentley-logo.png';
 import passportLogo from '../images/passport-logo.png';
 import lowesLogo from '../images/lowes-logo.png';
+import vimeoLogo from '../images/vimeo-logo.png';
 
 const Work = () => (
   <div
@@ -16,10 +16,18 @@ const Work = () => (
     <h1 className="f2 f1-m f1-l fw5 mt0 mb4 mb5-ns near-black">Work</h1>
     <div className="flex flex-wrap justify-center mw9">
       <a
-        href="https://www.lowes.com"
+        href="https://www.vimeo.com/ott"
         target="_blank"
         rel="noopener noreferrer"
         className="work-tile-link shadow-4 grow mb4 mr4-ns"
+      >
+        <img className="db" src={vimeoLogo} alt="Vimeo OTT website" />
+      </a>
+      <a
+        href="https://www.lowes.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="work-tile-link shadow-4 grow mb4 mr4-l"
       >
         <img className="db" src={lowesLogo} alt="Lowe's website" />
       </a>
@@ -27,7 +35,7 @@ const Work = () => (
         href="https://www.passportinc.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mr4-l"
+        className="work-tile-link shadow-4 grow mb4 mr4-ns"
       >
         <img className="db" src={passportLogo} alt="Passport website" />
       </a>
@@ -35,7 +43,7 @@ const Work = () => (
         href="https://www.epri.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mr4-ns"
+        className="work-tile-link shadow-4 grow mb4"
       >
         <img className="db" src={epriLogo} alt="EPRI website" />
       </a>
@@ -43,7 +51,7 @@ const Work = () => (
         href="https://www.jabil.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4"
+        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-ns"
       >
         <img className="db" src={jabilLogo} alt="Jabil website" />
       </a>
@@ -51,7 +59,7 @@ const Work = () => (
         href="https://www.quietstreamfinancial.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-ns"
+        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-l"
       >
         <img className="db" src={qsfLogo} alt="Quietstream website" />
       </a>
@@ -59,7 +67,7 @@ const Work = () => (
         href="https://www.bankofamerica.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-l"
+        className="work-tile-link shadow-4 grow mb4 mb0-m mb0-l mr4-ns"
       >
         <img className="db" src={boaLogo} alt="Bank of America website" />
       </a>
@@ -67,17 +75,9 @@ const Work = () => (
         href="https://www.atriumhealth.org"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-ns"
+        className="work-tile-link shadow-4 grow"
       >
         <img className="db" src={chsLogo} alt="CHS website" />
-      </a>
-      <a
-        href="https://www.bentley.com/en"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4-m"
-      >
-        <img className="db" src={bentleyLogo} alt="Bentley Systems website" />
       </a>
     </div>
   </div>
