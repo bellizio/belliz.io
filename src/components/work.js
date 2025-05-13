9ffc97bc311d9b8689654bd03ae9d99f1341309e
Devon Bellizio <jdbellizio@gmail.com>
2025-05-13 10:15:27 -0400

    update work

--- src/components/work.js
+++ src/components/work.js
@@ -7,6 +7,7 @@ import chsLogo from '../images/chs-logo.png';
 import passportLogo from '../images/passport-logo.png';
 import lowesLogo from '../images/lowes-logo.png';
 import vimeoLogo from '../images/vimeo-logo.png';
+import bitlyLogo from '../images/bitly-logo.png';
 
 const Work = () => (
   <div
@@ -14,70 +15,78 @@ const Work = () => (
     className="pv5 ph3 flex flex-column items-center justify-center page-section bg-light-gray"
   >
     <h1 className="f2 f1-m f1-l fw5 mt0 mb4 mb5-ns near-black">Work</h1>
-    <div className="flex flex-wrap justify-center mw9">
+    <div className="flex flex-wrap justify-center mw8">
+      <a
+        href="https://bitly.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="work-tile-link grow shadow-4 mr3-ns"
+      >
+        <img className="w-100" src={bitlyLogo} alt="Bitly website" />
+      </a>
       <a
         href="https://www.vimeo.com/ott"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mr4-ns"
+        className="work-tile-link grow shadow-4 mr3-ns mt3 mt0-ns"
       >
-        <img className="db" src={vimeoLogo} alt="Vimeo OTT website" />
+        <img className="w-100" src={vimeoLogo} alt="Vimeo website" />
       </a>
       <a
         href="https://www.lowes.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mr4-l"
+        className="work-tile-link grow shadow-4 mt3 mt0-ns"
       >
-        <img className="db" src={lowesLogo} alt="Lowe's website" />
+        <img src={lowesLogo} alt="Lowe's website" />
       </a>
       <a
         href="https://www.passportinc.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mr4-ns"
+        className="work-tile-link grow shadow-4 mr3-ns mt3"
       >
-        <img className="db" src={passportLogo} alt="Passport website" />
+        <img src={passportLogo} alt="Passport website" />
       </a>
       <a
         href="https://www.epri.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4"
+        className="work-tile-link grow shadow-4 mr3-ns mt3"
       >
-        <img className="db" src={epriLogo} alt="EPRI website" />
+        <img src={epriLogo} alt="EPRI website" />
       </a>
       <a
         href="https://www.jabil.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-ns"
+        className="work-tile-link grow shadow-4 mt3"
       >
-        <img className="db" src={jabilLogo} alt="Jabil website" />
+        <img src={jabilLogo} alt="Jabil website" />
       </a>
       <a
         href="https://www.quietstreamfinancial.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-l mr4-l"
+        className="work-tile-link grow shadow-4 mr3-ns mt3"
       >
-        <img className="db" src={qsfLogo} alt="Quietstream website" />
+        <img src={qsfLogo} alt="Quietstream website" />
       </a>
       <a
         href="https://www.bankofamerica.com"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow mb4 mb0-m mb0-l mr4-ns"
+        className="work-tile-link grow shadow-4 mr3-ns mt3"
       >
-        <img className="db" src={boaLogo} alt="Bank of America website" />
+        <img src={boaLogo} alt="Bank of America website" />
       </a>
       <a
         href="https://www.atriumhealth.org"
         target="_blank"
         rel="noopener noreferrer"
-        className="work-tile-link shadow-4 grow"
+        className="work-tile-link grow shadow-4 mt3"
       >
-        <img className="db" src={chsLogo} alt="CHS website" />
+        <img src={chsLogo} alt="Atrium Health website" />
       </a>
     </div>
   </div>
